3c610434f64b4b6694cf63354d101984a09e6967
eng-amroakram <eng.amroakram@gmail.com>
2023-06-06 11:06:42 +0300

    new update

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('plans', \App\Http\Controllers\API\PlanController::class)->only(['index']);
 Route::apiResource('subscriptions', \App\Http\Controllers\API\SubscriptionController::class)->only(['index', 'store']);
+Route::apiResource('exercises-testing', ExerciseController::class)->only(['index', 'show']);
 
 Route::group(['prefix' => 'v1', 'middleware' => ['jsonResponse', 'cors']], function () {
 
